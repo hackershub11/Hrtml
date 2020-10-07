7470125d32b2aea28b3750d31edceee302507e71
hackershub11 <72488167+hackershub11@users.noreply.github.com>
2020-10-07 12:17:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import Tweet from "./tweet"
+
+function App(){
+  const [isRed,setRed]=useState(false);
+  const [count,setCount]=useState(0);
+  
+
+  const Increment =() =>{
+    setCount(count+1);
+  }
+
+
+  return(
+    <div className="app">
+      <button onClick={Increment}>Increment</button>
+      <h1>{count}</h1>
+      <Tweet name="Puru" message="Hello World"/>
+      <Tweet name="DEv" message="Hello Puru"/>
+      <Tweet name="Joe" message="Hello World"/>
+      <Tweet name="Jaes"/>
+    </div>
+  );
+
+  }
+
+export default App;
